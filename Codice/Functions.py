65565ad62c00e09aeaedea1beda2ce96690280a7
Francesco La Rovere <franci192002@gmail.com>
2024-08-12 14:36:25 +0200

    update

--- Codice/Functions.py
+++ Codice/Functions.py
@@ -1,10 +1,12 @@
 from UtilityAndConst import *
 import uproot
+import matplotlib.pyplot as plt
+import mplhep as hep
+
+hep.style.use("CMS")
 
 
 #Funzioni
-def ConvertToHz(counts, n_ls = 1):
-  return counts / n_ls / ONE_LUMI_IN_S / 1e3
 
 def OpenRootFile(file, features):
   with uproot.open(file) as f:
@@ -12,3 +14,35 @@ def OpenRootFile(file, features):
     awk_array = evs.arrays(features, library="ak")
 
   return awk_array
+
+
+def ConvertToHz(counts, n_ls = 1):
+  return counts / n_ls / ONE_LUMI_IN_S / 1e3
+
+def draw_cms_label(ax: plt.Axes, label: str = "Preliminary", rlabel: str = "L1DS", fontsize: int = 28, data: bool = True):
+    
+    hep.cms.label(
+        ax       = ax,
+        data     = data,
+        label    = label,
+        rlabel   = rlabel,
+        fontsize = fontsize
+    )
+
+def set_label_font(ax: plt.Axes, fontsize: int = 28):
+    
+    ax.set_xlabel(ax.get_xlabel(), fontsize = fontsize)
+    ax.set_ylabel(ax.get_ylabel(), fontsize = fontsize)
+    
+    
+def set_tick_font(ax: plt.Axes, fontsize: int = 28):
+    
+    ax.tick_params(axis = "x", labelsize = fontsize, which = "major")
+    ax.tick_params(axis = "y", labelsize = fontsize, which = "major")
+    
+    
+def draw_grid(ax: plt.Axes, which: str = "major", axis: str = "both", alpha: float = 0.2, color: str = "gray", ls: str = "-"):
+    
+    ax.grid(True, which = which, axis = axis, alpha = alpha, color = color, ls = ls)
+    ax.set_axisbelow(True)
+
--- Codice/UtilityAndConst.py
+++ Codice/UtilityAndConst.py
@@ -1,15 +1,20 @@
-import uproot 
-import awkward as ak
-
 
 #Utility
 stub_features = ['orbit', 'bx', 'nStubs', 'stubHwPhi', 'stubHwPhiB', 'stubHwQual', 'stubHwEta', 'stubHwQEta', 'stubStation', 'stubWheel', 'stubSector', 'stubTag']
-bmtf_features = ['nBmtfMuons', 'bmtfHwPt', 'bmtfHwPtu', 'bmtfHwPhi', 'bmtfHwEta', 'bmtfPt', 'bmtfPtu', 'bmtfPhi', 'bmtfEta', 'bmtfHwSign', 'bmtfSignValid', 'bmtfDXY', 'bmtfHwQual', 'bmtfProcessor']
-gmt_features  = ['nGmtMuons', 'gmtHwPt', 'gmtHwPtu', 'gmtHwPhi', 'gmtHwEta', 'gmtHwPhiAtVtx', 'gmtHwEtaAtVtx', 'gmtPt', 'gmtPtu', 'gmtPhi', 'gmtEta', 'gmtPhiAtVtx', 'gmtEtaAtVtx', 'gmtCharge', 'gmtDXY', 'gmtQual', 'gmtTfIndex']
+bmtf_features = ['nBmtfMuons', 'bmtfHwPt', 'bmtfHwPtu', 'bmtfHwPhi', 'bmtfHwEta', 'bmtfPt', 'bmtfPtu', 'bmtfPhi', 'bmtfEta', 'bmtfHwSign', 'bmtfHwSignValid', 'bmtfHwDXY', 'bmtfHwQual', 'bmtfProcessor']
+gmt_features  = ['nGmtMuons', 'gmtHwPt', 'gmtHwPtu', 'gmtHwPhi', 'gmtHwEta', 'gmtHwPhiAtVtx', 'gmtHwEtaAtVtx', 'gmtPt', 'gmtPtu', 'gmtPhi', 'gmtEta', 'gmtPhiAtVtx', 'gmtEtaAtVtx', 'gmtCharge', 'gmtDxy', 'gmtQual', 'gmtTfIndex']
 
 
 
 #Costanti
+
+FONTSIZE  = 22
+FIGSIZE   = (10, 8)
+
+CMS_PALETTE_1 = ["#5790fc", "#f89c20", "#e42536", "#964a8b", "#9c9ca1", "#7a21dd"]
+CMS_PALETTE_2 = ["#3f90da", "#ffa90e", "#bd1f01", "#94a4a2", "#832db6", "#a96b59", "#e76300", "#b9ac70", "#717581", "#92dadd"]
+
+
 ONE_LUMI_IN_ORBITS = 2**18
 ONE_ORBIT_IN_BX    = 3564
 ONE_BX_IN_NS       = 25
